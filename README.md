4a857e5a5518ae6af825ed316ae4d91d2c2c98f6
ahmedsamyh <ahmedsamyh10@gmail.com>
2023-03-28 23:05:47 +0500

    Much more portable build script.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 *Actually slower than a std::vector at retrieving... probably i implemented it wrong...*
 
 i just watched one [video](https://www.youtube.com/watch?v=KyUTuwz_b7Q) and did this, maybe that's why
+
+
+# Dependencies:
+- msbuild (Add the msbuild exetuble found in [Visual Studio 2022](https://visualstudio.microsoft.com/downloads/) to the PATH environment variable.)
+- [premake5](https://premake.github.io/download/) (Also assumes to be on the PATH environment variable.)
+
+## Quick Start:
+```console
+$ build.bat Debug run
+```
--- build.bat
+++ build.bat
@@ -1,12 +1,77 @@
 @echo off
+
+set batchDir=%~dp0
 set config=%1
+set runPremake=1
+set runProg=%2
+
+if "%1"=="clean" (
+   if exist %batchDir%\bin (
+       rmdir /s /q  %batchDir%\bin
+       echo deleted .\bin\
+   )
+   if exist %batchDir%\build (
+       rmdir /s /q %batchDir%\build
+       echo deleted .\build\
+   )
+   
+   goto :eof
+)
+
+if "%1"=="help" (
+   echo Usage: build.bat ^<Build^|Release^> [subcommand]
+   echo.
+   echo Subcommands:
+   echo        help       Prints the usage.
+   echo        run        Runs the the first .exe file found in .\bin\^<Debug^|Release^>.
+   echo        clean      Removes .\bin\ and .\build\ if they exist.
+   echo.
+   echo NOTE:if only `run` subcommand is provided with no config the program will run without building.
+   goto :eof
+)
+
+
+if "%1"=="run" (
+    set runProg=run
+    set config=Debug
+    goto :run
+)
+
+
+rem run premake5
+if "%runPremake%"=="1" (
+   echo.
+   echo ^<=== Running Premake5 ===^>
+   echo.
+   premake5 --file=%batchDir%\premake5.lua vs2022
+)
 
 rem default condig is Debug
 if "%config%"=="" (
     set config=Debug
 )
 
-echo Building %config%....
+echo.
+echo ^<=== Building %config% ===^>
 echo.
 
-msbuild -p:configuration=%config% -warnAsMessage:LNK4217,LNK4286 -verbosity:m -m D:\programming\C++\hash-table\build\hash-table.sln
+msbuild -p:configuration=%config% -warnAsMessage:LNK4217,LNK4286 -verbosity:m -m %batchDir%build\hash-table.sln
+if not "%errorlevel%"=="0" ( exit %errorlevel% )
+
+:run
+if "%runProg%"=="run" (
+    if not exist %batchDir%bin (
+        echo ERROR: executable not found!
+	echo HINT: try to build once before running!
+        exit 1
+    )
+    for %%F in (%batchDir%bin\%config%\*) do (
+        if "%%~xF"==".exe" (
+            echo.
+            echo ^<=== RUNNING %%~nxF ===^>
+            echo.
+
+	    %%F
+	)
+    )
+)
